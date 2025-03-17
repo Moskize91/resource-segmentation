f09dc9ea8aa079f229f614dff78c612d3243246f
Moskize91 <i@taozeyu.com>
2025-03-17 10:58:00 +0800

    feat(project): migrate codes from pdf-craft & remove bussines logic (#1)

---
+++ resource_segmentation/__init__.py
@@ -0,0 +1,4 @@
+from .types import Resource
+from .splitter import split
+from .group import Group
+from .segment import Segment
---
+++ resource_segmentation/types.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from enum import IntEnum
+from typing import TypeVar, Generic
+
+
+P = TypeVar("P")
+
+@dataclass
+class Incision(IntEnum):
+  MUST_BE = 2
+  MOST_LIKELY = 1
+  IMPOSSIBLE = -1
+  UNCERTAIN = 0
+
+@dataclass
+class Resource(Generic[P]):
+  count: int
+  start_incision: Incision
+  end_incision: Incision
+  payload: P
